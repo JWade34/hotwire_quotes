5eac4cb8040e13f793677495c58ec0ea1d76ccaf
justin <justinwmckelvey@gmail.com>
2022-07-16 09:48:29 -0500

    add standard rb

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,56 +1,57 @@
 class QuotesController < ApplicationController
-    before_action :set_quote, only: [:show, :edit, :update, :destroy]
+  before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
-    def index
-      @quotes = Quote.ordered 
-    end
+  def index
+    @quotes = Quote.ordered
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def new
-      @quote = Quote.new
-    end
+  def new
+    @quote = Quote.new
+  end
 
-    def create
-      @quote = Quote.new(quote_params)
+  def create
+    @quote = Quote.new(quote_params)
 
-      if @quote.save
-        respond_to do |format|
-          format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-          format.turbo_stream
-        end
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-
-    def edit
-    end
-
-    def update
-      if @quote.update(quote_params)
-        redirect_to quotes_path, notice: "Quote was successfully updated."
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
-
-    def destroy
-      @quote.destroy
-      # redirect_to quotes_path, notice: "Quote was successfully destroyed."
+    if @quote.save
       respond_to do |format|
-        format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
         format.turbo_stream
       end
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    private
-    def set_quote
-      @quote = Quote.find(params[:id])
-    end
+  def edit
+  end
 
-    def quote_params
-      params.require(:quote).permit(:name)
+  def update
+    if @quote.update(quote_params)
+      redirect_to quotes_path, notice: "Quote was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @quote.destroy
+    # redirect_to quotes_path, notice: "Quote was successfully destroyed."
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quote_params
+    params.require(:quote).permit(:name)
+  end
 end
